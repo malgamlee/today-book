918c49113762f64ec1b98a6575912f79cb70f330
malgamlee <malgamlee@gmail.com>
2022-06-21 04:22:48 +0900

    fix. modify search component

--- src/routes/NewMyPage/index.tsx
+++ src/routes/NewMyPage/index.tsx
@@ -2,21 +2,12 @@ import styles from './myPage.module.scss'
 import { useRecoilValue } from 'recoil'
 import { likeStoreState } from 'states/storeState'
 import { Link } from 'react-router-dom'
-import { RowBookList } from 'components/RowBookList'
 
 const NewMyPage = () => {
   const likeStoreData = useRecoilValue(likeStoreState)
   console.log(likeStoreData)
 
-  return (
-    <div>
-      <div className={styles.partTitle}>
-        저자의 다른 도서
-        <Link to='../wishList'>+ 더보기</Link>
-      </div>
-      {/* <RowBookList bookList={likeStoreData} /> */}
-    </div>
-  )
+  return <div>폴딩</div>
 }
 
 export default NewMyPage
