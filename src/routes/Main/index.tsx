3084d5b5d7081d3a33b9d843ec57c2aee8131fca
malgamlee <malgamlee@gmail.com>
2022-07-14 16:41:59 +0900

    fix. modify package.json

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -3,12 +3,25 @@ import { useNavigate } from 'react-router-dom'
 import styles from './main.module.scss'
 import { RowBookList } from 'components/RowBookList'
 import { BookIcon } from 'assets/svgs'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
-const NewMain = () => {
+const Main = () => {
   const navigate = useNavigate()
   const handleEventClick = () => {
     navigate('bookcard')
   }
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+  }
+
   return (
     <div className={styles.main}>
       <div className={styles.banner}>
@@ -35,8 +48,37 @@ const NewMain = () => {
         베스트셀러
         <RowBookList bookList={bestSeller} />
       </div>
+      <div className={styles.test}>
+        <h2> Responsive </h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   )
 }
 
-export default NewMain
+export default Main
